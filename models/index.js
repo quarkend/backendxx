58e100a54e219d2cf3196e65f21ebbadc987d52c
hinafi <quarkend@live.fr>
2023-01-14 20:35:53 +0100

    railway

--- models/index.js
+++ models/index.js
@@ -13,8 +13,17 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 }
 
+ 
 fs
   .readdirSync(__dirname)
   .filter(file => {
