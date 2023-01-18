971f4fa972a093b05b3b60322f91b2e2258eb7d9
hinafi <quarkend@live.fr>
2023-01-18 10:11:46 +0100

    res.header( 'Access-Control-Allow-Credentials',true);

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
+    ( 'Access-Control-Allow-Credentials',true)
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
